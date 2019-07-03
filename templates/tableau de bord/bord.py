a357723ffb441b8bc7a7e4993d6dde1fdbaf1f0f
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-03 23:43:33 +0200

    Add files via upload

---
+++ templates/tableau de bord/bord.py
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+  <html lang="fr">
+
+    <!-- Head section - Loading CDN -->
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta name="description" content="">
+      <meta name="jb" content="">
+      <title>Veille Technologique</title>
+    
+
+      <!-- Google Fonts -->
+      <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet">
+      <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
+
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+      <!-- Theme CSS - Includes Bootstrap -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.js"></script>
+    </head>
+
+
+    <!-- Body section -->
+
+    <body>
+
+    </body>
+</html>
